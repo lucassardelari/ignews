59c604bf30fa0d27a448842ce26b903af31922f9
= <lucas_sardelari@hotmail.com>
2022-02-20 21:03:57 -0300

    feat: Add titulo por page de forma dinamica

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>ig.news</title>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
                 </Head>
