9f3a7bb34e5e15b7a0698b9c016aba31b66967d8
= <lucas_sardelari@hotmail.com>
2022-02-22 17:33:45 -0300

    feat: Config FaunaDB with create register new user verify it exists in FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,6 +12,40 @@ export default NextAuth({
       authorization: { params: { scope: 'read:user' }}
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail = user.email;
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(userEmail)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: userEmail } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(userEmail)
+              )
+            )
+          ),          
+        );
+        
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
 
 // FaunaDB - Melhor banco para se utilizar com Serveless
