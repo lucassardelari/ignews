a47594794be0cea5c1692eee0c13f76b7c433d93
= <lucas_sardelari@hotmail.com>
2022-02-23 00:18:32 -0300

    feat: Generate session checkout, redirect stripe, avoid duplicate data

--- src/components/SubscriveButton/index.tsx
+++ src/components/SubscriveButton/index.tsx
@@ -1,14 +1,50 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+// gerserversideprops
+// getstaticprops
+// API Routes
+
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
+    const {data: session } = useSession();
+    
+    
+     async function handleSubscribe(){
+        if (!session)
+        {
+            signIn('github')
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId})
+        }
+        catch (err)
+        {
+            alert(err.message);
+        }
+
+
+        // criacao da checkout session - stripe
+
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
